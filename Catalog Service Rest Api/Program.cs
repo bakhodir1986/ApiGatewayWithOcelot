da5171ae7ffc6d6211459cf310e029c3fa8221ea
Bakhodir Radjabov <bakhodir.radjabov@gmail.com>
2022-12-08 18:32:12 +0500

    Initial commit

---
+++ Catalog Service Rest Api/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUrlHelper, UrlHelper>(implementationFactory =>
+{
+    var actionContext = implementationFactory.GetService<IActionContextAccessor>().ActionContext;
+    return new UrlHelper(actionContext);
+
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
