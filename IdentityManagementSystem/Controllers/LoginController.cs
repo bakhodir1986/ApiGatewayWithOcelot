31514cbdbcede1d4045db550aa42028dedb938bd
Bakhodir Radjabov <bakhodir.radjabov@gmail.com>
2022-12-08 18:46:22 +0500

    Implement Identity

---
+++ IdentityManagementSystem/Controllers/LoginController.cs
@@ -0,0 +1,120 @@
+﻿using IdentityManagementSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IIdentityRepository _repository;
+
+        public LoginController(IIdentityRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] User userLogin)
+        {
+            var user = Authenticate(userLogin);
+
+            if (user != null)
+            {
+                var token = Generate(user);
+                return Ok(token);
+            }
+
+            return NotFound("User not found");
+        }
+
+        private string Generate(UserModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("DhftOS5uphK3vmCJQrexST1RsyjZBjXWRgJMFPU4"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            
+            List<Claim> claimsPermission = new List<Claim>();
+            foreach (var action in user.Permissions)
+            {
+                claimsPermission.Add(new Claim("Permission", action));
+            }
+
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.UserName),
+                new Claim(ClaimTypes.Role, user.RoleName)
+            };
+
+            claims.AddRange(claimsPermission);
+
+            var token = new JwtSecurityToken("https://localhost:47469/",
+              "https://localhost:47469/",
+              claims.ToArray(),
+              expires: DateTime.Now.AddMinutes(15),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserModel? Authenticate(User userLogin)
+        {
+
+            var currentUser = _repository.GetUser(userLogin.UserName, userLogin.Password);
+
+            if (currentUser != null)
+            {
+                return currentUser;
+            }
+
+            return null;
+        }
+
+        [HttpGet("Manager")]
+        [Authorize(Roles = "Manager")]
+        public IActionResult AdminsEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi {currentUser.UserName}, you are an {currentUser.RoleName}");
+        }
+
+
+        [HttpGet("Buyer")]
+        [Authorize(Roles = "Buyer")]
+        public IActionResult SellersEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi {currentUser.UserName}, you are a {currentUser.RoleName}");
+        }
+
+        private UserModel GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+
+                return new UserModel
+                {
+                    UserName = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value,
+                    RoleName = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value
+                };
+            }
+            return null;
+        }
+    }
+}
